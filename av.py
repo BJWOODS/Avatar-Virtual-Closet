e3eb726cae2c065c25fa57e3407494b7a81aa538
Luigi <ldantonio@csumb.edu>
2017-04-08 17:16:24 +0000

    avatar

---
+++ av.py
@@ -0,0 +1,11 @@
+from flask import *
+app = Flask(__name__)
+
+#app.run(host='0.0.0.0')
+
+@app.route('/')
+def index():
+    return render_template('hello.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
