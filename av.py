0ed0028da9c19e2210b0a00080d0a1130bbd0c39
Luigi <ldantonio@csumb.edu>
2017-04-08 17:45:01 +0000

    Avatar done

--- av.py
+++ av.py
@@ -1,11 +1,9 @@
-from flask import *
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-#app.run(host='0.0.0.0')
-
-@app.route('/')
-def index():
+@app.route('/index', methods=['GET', 'POST'])
+def lionel(): 
     return render_template('hello.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run()
